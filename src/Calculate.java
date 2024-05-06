a344a02b80e4333fa282ca5127f95b96fd8c81ba
Zhabotynska_Sofiia <117444366+SophiaZhab@users.noreply.github.com>
2024-05-06 11:30:00 +0300

    Update Calculate.java

--- src/Calculate.java
+++ src/Calculate.java
@@ -5,14 +5,14 @@
  */
 public class Calculate {
      /**
-     * Перемножує дві матриці `matrix_A` та `matrix_B` та зберігає результат у `matrix_C`.
+     * Перемножує дві матриці `matrix_A` та `matrix_B` та зберігає результат у `matrix_AxB`.
      * Розміри матриць повинні бути сумісними для множення:
      * кількість стовпців `matrix_A` повинна дорівнювати кількості рядків `matrix_B`.
      *
      * @param matrix_A перша матриця
      * @param matrix_B друга матриця
-     * @param matrix_C матриця для зберігання результату множення
-     * @return матриця `matrix_C` з результатом множення `matrix_A` та `matrix_B`
+     * @param matrix_AxB матриця для зберігання результату множення
+     * @return матриця `matrix_AxB` з результатом множення `matrix_A` та `matrix_B`
      */
     public static int[][] multiplicateMatrixs(int[][] matrix_A, int[][] matrix_B, int[][] matrix_AxB) {
         
